15ded40d3b8db13b620463e4a227aaf4ab54323e
CGGbang <57607833+CGGbang@users.noreply.github.com>
2020-12-14 21:21:34 +0800

    Update 评价.js

--- js/评价.js
+++ js/评价.js
@@ -1,6 +1,5 @@
 var btn = document.getElementById("btn");
 var choiceList = document.getElementsByClassName("choice");
-
 btn.onclick = function(){
     if(choiceList[0].checked){
         alert("谢谢好评");
@@ -10,9 +9,14 @@ btn.onclick = function(){
     }
     else if(choiceList[2].checked){
         alert("对于您的差评我们感到十分抱歉，我们会努力改善");
+        if(confirm("您愿意写下不满意的原因吗？")){
+            var text = prompt("请写下不满意的原因");
+            if( text != ""){
+                alert("对于您关于"+ text +"的反馈我们会及时处理");
+            }
+        }
     }
     else{
         alert("您还没有评价");
     }
-
 }
